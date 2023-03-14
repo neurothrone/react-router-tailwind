385adf658a8f4b3b8438cbb9a9bbda487de498b6
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-03-14 06:23:09 +0100

    add products and product page and set up routes

---
+++ src/Routes.tsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./App";
+import { ProductsPage } from "./pages/ProductsPage";
+import { ProductPage } from "./pages/ProductPage";
+import { ErrorPage } from "./pages/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "products",
+        element: <ProductsPage />,
+      },
+      {
+        path: "products/:id",
+        element: <ProductPage />,
+      },
+    ],
+  },
+]);
+
+export function Routes() {
+  return <RouterProvider router={router} />;
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+import logo from "../logo.svg";
+
+export function Header() {
+  return (
+    <header
+      className="text-center text-slate-50
+      bg-slate-900 h-40 p-5"
+    >
+      <img src={logo} alt="Logo" className="inline-block h-20" />
+      <h1 className="text-2xl">React Tools</h1>
+      <nav>
+        <NavLink
+          to="products"
+          className={({ isActive }) =>
+            `text-white no-underline p-1 pb-0.5 border-solid border-b-2 ${
+              isActive ? "border-white" : "border-transparent"
+            }`
+          }
+        >
+          Products
+        </NavLink>
+      </nav>
+    </header>
+  );
+}
